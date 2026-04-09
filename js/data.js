6342b9ea262063d595dd7ca72f6501a93bec8346
danzlh94 <danzlh94@gmail.com>
2026-04-09 16:33:43 +0700

    Update image URLs for beverage items

--- js/data.js
+++ js/data.js
@@ -3,19 +3,19 @@ const products = [
     id: 1,
     name: "Phin Sữa Đá",
     price: 35000,
-    img: "images/phin_sua_da.jpg"
+    img: "https://static.wixstatic.com/media/bdc8a3_9c7c8a0454194bcb803d4d7a64b0807f~mv2.jpg/v1/fill/w_480,h_480,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/bdc8a3_9c7c8a0454194bcb803d4d7a64b0807f~mv2.jpg"
   },
   {
     id: 2,
     name: "Bạc Xỉu",
     price: 39000,
-    img: "images/bac_xiu.jpg"
+    img: "https://visty.vn/wp-content/uploads/2021/07/7-mon-ca-phe-tu-pha-tai-nha-bac-xiu-ca-phe-1216x800.jpg"
   },
   {
     id: 3,
     name: "Trà Sen Vàng",
     price: 45000,
-    img: "images/tra_sen_vang.jpg"
+    img: "https://cf.shopee.vn/file/7ce3afae60cb1a6a80356fa7a5810c80"
   },
   {
     id: 4,
