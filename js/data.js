aed4b5a7df959256adef52e8cf1ed431713bc123
danzlh94 <danzlh94@gmail.com>
2026-04-09 16:08:38 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -3,13 +3,31 @@ const products = [
     id: 1,
     name: "Phin Sữa Đá",
     price: 35000,
-    img: "https://images.unsplash.com/photo-1552058544-f2b08422138a"
+    img: "images/phin_sua_da.jpg"
   },
   {
     id: 2,
+    name: "Bạc Xỉu",
+    price: 39000,
+    img: "images/bac_xiu.jpg"
+  },
+  {
+    id: 3,
     name: "Trà Sen Vàng",
-    price: 40000,
-    img: "https://images.unsplash.com/photo-1559561854-8c3ecb2d2cea"
+    price: 45000,
+    img: "images/tra_sen_vang.jpg"
+  },
+  {
+    id: 4,
+    name: "Trà Đào Cam Sả",
+    price: 48000,
+    img: "images/tra_dao.jpg"
+  },
+  {
+    id: 5,
+    name: "Cold Brew",
+    price: 55000,
+    img: "images/cold_brew.jpg"
   }
 ];
 
@@ -19,10 +37,10 @@ if (productList) {
   products.forEach(p => {
     productList.innerHTML += `
       <div class="card">
-        <img src="${p.img}">
+        <img src="${p.img}" alt="${p.name}">
         <h3>${p.name}</h3>
-        <p>${p.price} đ</p>
-        <button onclick="addToCart(${p.id})">Mua</button>
+        <p class="price">${p.price.toLocaleString()} đ</p>
+        <button onclick="addToCart(${p.id})">Thêm vào giỏ</button>
       </div>
     `;
   });
